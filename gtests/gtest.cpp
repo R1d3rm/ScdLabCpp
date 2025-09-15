3af73304814eb53c7b5b00312462cc0996772d8f
riderm <2006morozov@bk.ru>
2025-09-15 20:36:20 +0700

    modify gtest

--- gtests/gtest.cpp
+++ gtests/gtest.cpp
@@ -2,11 +2,11 @@
 #include<string>
 #include<gtest/gtest.h>
 #include"../head/base.hpp"
-TEST(split,test1){
+TEST(firstSplit,test1){
     std::vector<std::string> ans{"101.105.104.255","0","1"};
     EXPECT_EQ(split("101.105.104.255	0	1",'\t'), ans);
 }
-TEST(split,test2){
+TEST(secondSplit,test2){
     std::vector<std::string> a{"113", "162", "145", "156"};
     EXPECT_EQ(split("113.162.145.156", '.'), a);
 }
