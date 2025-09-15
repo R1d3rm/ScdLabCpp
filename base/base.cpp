e770a9900e69121b9c493480402817146d4ef9ab
riderm <2006morozov@bk.ru>
2025-09-15 20:54:11 +0700

    i don't know what to do

--- base/base.cpp
+++ base/base.cpp
@@ -6,7 +6,7 @@ std::vector<std::string> split(const std::string &str, char d)
     std::vector<std::string> r;
 
     std::string::size_type start = 0;
-    decltype(start) stop = str.find_first_of(d); // здесь decltype
+    decltype(start) stop = str.find_first_of(d);
     while(stop != std::string::npos)
     {
         r.push_back(str.substr(start, stop - start));
